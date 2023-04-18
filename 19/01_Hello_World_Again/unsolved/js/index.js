5182ed5205295b60ebae96247b1148441dc99451
MohamedElmi7 <119367900+MohamedElmi7@users.noreply.github.com>
2023-04-17 19:40:06 -0500

    activities

--- 19/01_Hello_World_Again/unsolved/js/index.js
+++ 19/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,4 @@
 // Create a variable below
-
+var helloworld = "Hello World";
 // Console.log() your variable here.
+console.log(helloworld);
--- 19/02_More_Variables/unsolved/js/index.js
+++ 19/02_More_Variables/unsolved/js/index.js
@@ -1,3 +1,8 @@
 // Create the addNumbers function here.
+function addnumbers() { var two = 2; 
+var three = 3;
+var five = two + three;
+console.log(five);
+}
 
 // Create the stringConcat function here.
